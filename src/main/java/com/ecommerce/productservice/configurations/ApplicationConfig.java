eecdb86f56d0fc60ac051374c5f91474dd1a3500
nrk8008 <nrk8008@gmail.com>
2024-12-16 20:03:18 +0530

    updated

--- src/main/java/com/ecommerce/productservice/configurations/ApplicationConfig.java
+++ src/main/java/com/ecommerce/productservice/configurations/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfig {
-    @Bean // Denotes that this method is a bean producer
+    @Bean //this method is a bean producer
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
